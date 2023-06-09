e5339c948f67bf5622b779bb5cc291a0cb14f84e
iamsesshie <msesssie@gmail.com>
2023-06-09 13:46:13 +0200

    entrer du gitignore

---
+++ Untitled-2.py
@@ -0,0 +1,47 @@
+"""premier programme python
+demande de nom et de l age de l utilisateur"""
+
+"""nom=input("quel est votre nom ? ")
+age=input("quel est votre age ? ")
+print("votre nom est " +nom+ " et vous avez "+age+ " ans")"""
+
+#qppel de fonction
+"""def fonctionage():
+    #age=0
+    """while age==0:
+        agestr=input("entrer votre age")
+        try:
+            age=int(# `agestr` is a variable that stores the user input for their age as a string. It
+            # is used in the `try-except` block to attempt to convert the string to an integer
+            # and assign it to the variable `age`.
+            agestr)
+        except:
+            print("ERREUR = entrer votre age")"""
+    #return age
+
+"""#nom="marius"
+#age=20
+#contrainte d age
+"""
+#demander le nom
+"""nom=""
+while nom =="":
+    nom=input("quel est votre nom ? ")"""
+
+
+#demander l age
+#fonctionage()
+# a corriger
+#lanprochain= str(age) + 1
+
+#print("vous vous appelez "+nom+ " ,vous avez "+ str(age) + "ans")
+
+#print(type(nom))
+#print(type(age))
+
+nom=print("entrer voter nom")
+age=print("entrer votre age")
+print("votre nom est " + nom +" et voter age est " + age + "ans")
+
+
+
